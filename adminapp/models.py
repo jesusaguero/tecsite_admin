d151d30556d7ea8d14a3048c60ec8aa0f3e4af77
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 13:24:52 -0500

    update administrator

--- adminapp/models.py
+++ adminapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Usuario(models.Model):
-    codigo = models.CharField(max_length=255, unique=True)
-    contrasena = models.CharField(max_length=255)
+    codigo = models.CharField(max_length=50, unique=True)
+    contrasena = models.CharField(max_length=25)
 
     def __str__(self):
         return self.codigo
