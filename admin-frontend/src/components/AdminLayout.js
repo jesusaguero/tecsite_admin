939ba39bc8fa1aee3fb2a1fed12f4a9b22d4025c
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 16:30:12 -0500

    admin dashboard

--- admin-frontend/src/components/AdminLayout.js
+++ admin-frontend/src/components/AdminLayout.js
@@ -8,14 +8,14 @@ const AdminLayout = ({ children }) => {
                     <div className="container">
                         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                             <a href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                                <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"/></svg>
+                                <svg className="bi me-2" width="40" height="32" role="img" aria-label="TECSITE"><use/></svg>
                             </a>
 
                             <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                                 <li>
                                     <a href="#" className="nav-link text-secondary">
-                                        <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#home"/></svg>
-                                        Home
+                                        <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#Admin"/></svg>
+                                        Admin
                                     </a>
                                 </li>
                                 <li>
@@ -46,7 +46,24 @@ const AdminLayout = ({ children }) => {
                             </div>
                         </div>
                     </div>
-                </header>
+            </header>
+            
+            <div class="container">
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <div class="col-md-4 d-flex align-items-center">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+                    </a>
+                    <span class="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
+                    </div>
+
+                    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+                    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+                    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+                    </ul>
+                </footer>
+            </div>
                 <main>{children}</main>
             </div>
     );
