93f3a131a145f5cb2494717e4a2c26be381ecefe
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 12:09:31 -0500

    API

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// admin-frontend/src/App.js
+
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Login from './components/Login'; // crea este componente
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        {/* Otras rutas para el CRUD y otras vistas */}
+      </Switch>
+    </Router>
   );
 }
 
