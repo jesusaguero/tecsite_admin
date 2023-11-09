a09ea1bcdf2509fb6d9ab7217bfcf46c169b6fee
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 16:35:57 -0500

    update

--- admin-frontend/src/components/AdminPanel.js
+++ admin-frontend/src/components/AdminPanel.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
+import AdminLayout from "./AdminLayout";
 
 const AdminPanel = () => {
-  const [nombrePolideportivo, setNombrePolideportivo] = useState('');
+    const [nombrePolideportivo, setNombrePolideportivo] = useState('');
 
-  const handleAgregarPolideportivo = () => {
-    console.log('Agregar polideportivo:', nombrePolideportivo);
-  };
+    const handleAgregarPolideportivo = () => {
+        console.log('Agregar polideportivo:', nombrePolideportivo);
+    };
 
-  return (
-    <div>
-      <h2>Panel de Administración</h2>
-      <form>
-        <label>
-          Nombre del Polideportivo:
-          <input
-            type="text"
-            value={nombrePolideportivo}
-            onChange={(e) => setNombrePolideportivo(e.target.value)}
-          />
-        </label>
-        <button type="button" onClick={handleAgregarPolideportivo}>
-          Agregar Polideportivo
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <AdminLayout>
+            <form>
+                <label>
+                    Nombre del Polideportivo:
+                    <input
+                        type="text"
+                        value={nombrePolideportivo}
+                        onChange={(e) => setNombrePolideportivo(e.target.value)}
+                    />
+                </label>
+                <button type="button" onClick={handleAgregarPolideportivo}>
+                    Agregar Polideportivo
+                </button>
+            </form>
+        </AdminLayout>
+    );
 };
 
 export default AdminPanel;
