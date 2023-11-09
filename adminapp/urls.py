afedc6bbbaf91075989877caa9225cc6e3a317f8
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 12:16:29 -0500

    update

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,5 +4,4 @@ from .views import login_view
 
 urlpatterns = [
     path('api/login/', login_view, name='login'),
-    # otras URLS para el CRUD y otras vistas
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,16 +1,22 @@
+import json
+
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 
+@csrf_exempt
+@require_POST
 def login_view(request):
-    if request.method == 'POST':
-        codigo = request.POST.get('codigo')
-        contrasena = request.POST.get('contrasena')
-        
-        user = authenticate(request, codigo=codigo, password=contrasena)
-        
-        if user is not None:
-            login(request, user)
-            return JsonResponse({'success': True})
-        else:
-            return JsonResponse({'success': False, 'error': 'Código o contraseña incorrectos'})
+    data = json.loads(request.body)
+    codigo = data.get('codigo', '')
+    contrasena = data.get('contrasena', '')
+    
+    user = authenticate(request, codigo=codigo, password=contrasena)
+    
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'error': 'Código o contraseña incorrectos'})
