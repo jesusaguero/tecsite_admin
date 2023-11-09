6ab38b3a621f8fe865aa53d66943e35ae8f48da2
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 16:31:17 -0500

    librerias

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AdminPanel from './components/AdminPanel';
 function App() {
