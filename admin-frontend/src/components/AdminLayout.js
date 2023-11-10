8dca5aa86a34fdaab25be05accd5dbd975482162
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 22:16:48 -0500

    admin

--- admin-frontend/src/components/AdminLayout.js
+++ admin-frontend/src/components/AdminLayout.js
@@ -3,17 +3,73 @@ import React from 'react';
 const AdminLayout = ({ children }) => {
     return (
         <div className="admin-layout">
-        <header className="bg-dark text-white">
-            <div className="container py-2">
-                <div className="d-flex flex-wrap align-items-center justify-content-center">
-                    <a href="/" className="d-flex align-items-center text-white text-decoration-none">
-                        <img src="/logo.png" alt="TECSITE Logo" width="120" height="120" className="logo" />
-                    </a>
+            <header className="bg-dark text-white">
+                <div className="container py-2">
+                    <div className="d-flex flex-wrap align-items-center justify-content-center">
+                        <a href="/" className="d-flex align-items-center text-white text-decoration-none">
+                            <img src="/logo.png" alt="TECSITE Logo" width="120" height="120" className="logo" />
+                        </a>
+                    </div>
+                </div>
+            </header>
+            
+            <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                <h1 className="display-4">Pricing</h1>
+                <p className="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+            </div>
+            
+            <div className="container">
+                <div className="card-deck mb-3 text-center">
+                    <div className="card mb-4 box-shadow">
+                        <div className="card-header">
+                            <h4 className="my-0 font-weight-normal">Free</h4>
+                        </div>
+                        <div className="card-body">
+                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
+                            <ul className="list-unstyled mt-3 mb-4">
+                                <li>10 users included</li>
+                                <li>2 GB of storage</li>
+                                <li>Email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                        </div>
+                    </div>
+                    <div class="card mb-4 box-shadow">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">Pro</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                            <li>20 users included</li>
+                            <li>10 GB of storage</li>
+                            <li>Priority email support</li>
+                            <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+                        </div>
+                    </div>
+                    <div class="card mb-4 box-shadow">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                            <li>30 users included</li>
+                            <li>15 GB of storage</li>
+                            <li>Phone and email support</li>
+                            <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </header>
+
             <main>{children}</main>
-            </div>
+        </div>
     );
 };
 
