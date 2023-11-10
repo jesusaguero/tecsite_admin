2fd1577b72f53b41dda24c424526d7b9d4e6efbd
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 23:20:29 -0500

    update

--- admin-frontend/src/components/AdminLayout.js
+++ admin-frontend/src/components/AdminLayout.js
@@ -14,57 +14,53 @@ const AdminLayout = ({ children }) => {
             </header>
             
             <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 className="display-4">Pricing</h1>
-                <p className="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+                <h1 className="display-4">Bienvenido</h1>
+                <p className="lead">Gracias por formar parte del TEAM TECSITE by Tecsup.</p>
             </div>
             
             <div className="container">
                 <div className="card-deck mb-3 text-center">
-                    <div className="card mb-4 box-shadow">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                        </div>
-                    </div>
-                    <div class="card mb-4 box-shadow">
-                        <div class="card-header">
-                            <h4 class="my-0 font-weight-normal">Pro</h4>
-                        </div>
-                        <div class="card-body">
-                            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-                            <ul class="list-unstyled mt-3 mb-4">
-                            <li>20 users included</li>
-                            <li>10 GB of storage</li>
-                            <li>Priority email support</li>
-                            <li>Help center access</li>
-                            </ul>
-                            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-                        </div>
-                    </div>
-                    <div class="card mb-4 box-shadow">
-                        <div class="card-header">
-                            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-                        </div>
-                        <div class="card-body">
-                            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-                            <ul class="list-unstyled mt-3 mb-4">
-                            <li>30 users included</li>
-                            <li>15 GB of storage</li>
-                            <li>Phone and email support</li>
-                            <li>Help center access</li>
-                            </ul>
-                            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-                        </div>
-                    </div>
+                <div className="card mb-4 box-shadow">
+                <div className="card-header">
+                    <h4 className="my-0 font-weight-normal">POLIDEPORTIVOS</h4>
+                </div>
+                <div className="card-body text-center">
+                    <ul className="list-unstyled mt-3 mb-4">
+                        <img src="/polideportivo.png" alt="TECSITE Logo" width="400" height="300" className="logo" />
+                    </ul>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-5 btn-separator">Ver Lista</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3 btn-separator">Agregar</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3">Actualizar</button>
+                </div>
+            </div>
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">LABORATORIOS</h4>
+                </div>
+                <div className="card-body text-center">
+                    <ul class="list-unstyled mt-3 mb-4">
+                    <img src="/aulas.png" alt="TECSITE Logo" width="400" height="300" className="logo" />
+                    </ul>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-5 btn-separator">Ver Lista</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3 btn-separator">Agregar</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3">Actualizar</button>
+                </div>
+            </div>
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">USUARIOS</h4>
+                </div>
+                <div className="card-body text-center">
+                <ul className="list-unstyled mt-3 mb-4">
+                    <img src="/alumnos.png" alt="TECSITE Logo" width="400" height="300" className="logo" />
+                </ul>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-5 btn-separator">Ver Lista</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3 btn-separator">Agregar</button>
+                    <button type="button" className="btn btn-lg btn-block btn-primary mt-3">Actualizar</button>
+            </div>
+
+
+            </div>
                 </div>
             </div>
             <footer className="bg-dark text-white">
