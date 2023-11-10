9a27aad505fca5247e71e786b988c82598363580
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-09 22:18:10 -0500

    update

--- admin-frontend/src/components/AdminLayout.js
+++ admin-frontend/src/components/AdminLayout.js
@@ -67,7 +67,15 @@ const AdminLayout = ({ children }) => {
                     </div>
                 </div>
             </div>
-
+            <footer className="bg-dark text-white">
+                <div className="container py-2">
+                    <div className="d-flex flex-wrap align-items-center justify-content-center">
+                        <a href="/" className="d-flex align-items-center text-white text-decoration-none">
+                            <img src="/logo.png" alt="TECSITE Logo" width="120" height="120" className="logo" />
+                        </a>
+                    </div>
+                </div>
+            </footer>
             <main>{children}</main>
         </div>
     );
