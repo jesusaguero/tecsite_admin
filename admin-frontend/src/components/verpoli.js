7b33b390e4eb4566866aa11b576805182d2fac3e
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-11-11 17:36:06 -0500

    estilos admin

--- admin-frontend/src/components/verpoli.js
+++ admin-frontend/src/components/verpoli.js
@@ -18,6 +18,15 @@ const Verpoli = () => {
 
   return (
     <div>
+    <header className="bg-dark text-white">
+        <div className="container py-2">
+            <div className="d-flex flex-wrap align-items-center justify-content-center">
+                <a href="/" className="d-flex align-items-center text-white text-decoration-none">
+                    <img src="/logo.png" alt="TECSITE Logo" width="120" height="120" className="logo" />
+                </a>
+            </div>
+        </div>
+    </header>
       <h1>Lista de Polideportivos</h1>
       <table>
         <thead>
