b57ee5aad14d530634f21e5519a7e7d9c0c86cdf
jesusaguero <jesus.aguero@tecsup.edu.pe>
2023-12-11 19:27:31 -0500

    ADMIN-CONFIG

--- adminapp/serializers.py
+++ adminapp/serializers.py
@@ -31,9 +31,9 @@ class HorarioSerializer(serializers.ModelSerializer):
 class ReservaLaboratorioSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReservaLaboratorio
-        fields = ['laboratorio.nombre', 'fecha', 'horario', 'fecha_fin', 'fecha_inicio']
+        fields = ['laboratorio', 'fecha', 'Horario']
 
 class ReservaPolideportivoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReservaPolideportivo
-        fields = ['polideportivo.nombre', 'fecha', 'horario', 'fecha_fin', 'fecha_inicio']
+        fields = ['polideportivo', 'fecha', 'Horario']
